cb5c09d105055247113685d6687f62a45ff01c93
Akacja <tomasz.traczyk@onet.eu>
2019-06-26 20:59:35 +0200

    Cleanup JSonHelper from unnecessary code

--- Belfer/DataBaseContext/JSonHelper.cs
+++ Belfer/DataBaseContext/JSonHelper.cs
@@ -28,59 +28,11 @@ namespace Belfer.DataBaseContext
             try
             {
                 return JsonConvert.DeserializeObject<ConnectionParams>(File.ReadAllText(ConfigFilePath));
-                //using (StreamReader SR = new StreamReader(ConfigFilePath))
-                //{
-                //	var JS = new JsonSerializer();
-                //                TargetObject = (ConnectionParams)JS.Deserialize(SR, typeof(ConnectionParams));
-                //                return true;
-                //}
             }
             catch (System.Exception)
             {
                 throw;
             }
         }
-        //public static JObject ReadConfigFile(string ConfigFilePath)
-        //{
-        //    try
-        //    {
-        //        using (StreamReader SR = new StreamReader(ConfigFilePath))
-        //        {
-        //            JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(SR));
-        //            return o;
-        //        }
-        //    }
-        //    catch (System.Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
-        //public static JsonSchema GetSchema(System.Type ObjectType)
-        //{
-        //    try
-        //    {
-        //        var SchemaGenerator = new JsonSchemaGenerator();
-        //        var schema = SchemaGenerator.Generate(ObjectType);
-        //        schema.Title = ObjectType.Name;
-        //        schema.Type = JsonSchemaType.Object;
-        //        return schema;
-        //    }
-        //    catch (System.Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
-        //public static bool ValidateParams(JObject CP, System.Type ObjectType)
-        //{
-        //    try
-        //    {
-        //        JsonSchema schema = GetSchema(ObjectType);
-        //        return CP.IsValid(schema);
-        //    }
-        //    catch (System.Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
     }
 }
